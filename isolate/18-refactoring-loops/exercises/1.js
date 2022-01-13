6a29e63a5769f6cc9906f621a3f0e037282e78d9
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-13 10:07:41 +0100

    Update 1.js

--- isolate/18-refactoring-loops/exercises/1.js
+++ isolate/18-refactoring-loops/exercises/1.js
@@ -11,10 +11,17 @@ for (const letter of school) {
 
 // write this for...of loop as a for loop:
 
-for (_; _; _) {}
+for (let i = 0; i < school.length; i++) {
+  let letter = school[i];
+  console.log(letter);
+}
 
 // write this for loop as a while loop:
 
-while (_) {}
+let letter = 0;
+while (letter < school.length) {
+  console.log(school[letter]);
+  letter++;
+}
 
 console.log('-- end --');
